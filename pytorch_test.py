969a1ca6ff50758be264b092d694c370e7cda09a
Leon <73463335+Leon66666666666@users.noreply.github.com>
2021-07-09 21:13:19 +0100

    Add files via upload

---
+++ pytorch_test.py
@@ -0,0 +1,82 @@
+from pytorch_segnet import *
+import sys
+import csv
+
+
+def test(SegNet):
+
+    SegNet.load_state_dict(torch.load(WEIGHTS,map_location=torch.device('cpu')))
+    SegNet.eval()
+
+    paths = os.listdir(SAMPLES)
+
+    for path in paths:
+
+        image_src = cv.imread(SAMPLES + path)
+        image = cv.resize(image_src, (416, 416))
+
+        image = image / 255.0
+        image = torch.Tensor(image)
+        image = image.permute(2, 0, 1)
+        image = torch.unsqueeze(image, dim=0)
+
+        output, relu_value = SegNet(image)
+        output = torch.squeeze(output)
+        output = output.argmax(dim=0)
+        output_np = cv.resize(np.uint8(output), (2048, 1024))
+
+        image_seg = np.zeros((1024, 2048, 3))
+        image_seg = np.uint8(image_seg)
+        mask = np.zeros((1024, 2048, 3))
+        mask = np.uint8(mask)
+        label = ['road','sidewalk','building','wall','fence','pole','traffic light','traffic sign','vegetation','terrain','sky'
+                 ,'person','rider','car','truck','bus','train','motocycle','bicycle']
+
+        colors = COLORS
+
+        for c in range(CLASS_NUM):
+            image_seg[:, :, 0] += np.uint8((output_np == c)) * np.uint8(colors[c][0])
+            image_seg[:, :, 1] += np.uint8((output_np == c)) * np.uint8(colors[c][1])
+            image_seg[:, :, 2] += np.uint8((output_np == c)) * np.uint8(colors[c][2])
+
+
+        with open("./user_info.csv","w",newline='') as f:
+            writer=csv.writer(f)
+            for column in image_seg:
+                writer.writerow(column)
+        image_seg = Image.fromarray(np.uint8(image_seg))
+
+
+        image_src = cv.cvtColor(image_src,cv.COLOR_BGR2RGB)
+        old_image = Image.fromarray(np.uint8(image_src))
+
+        image = Image.blend(old_image, image_seg, 0.6)
+
+        # 将背景或空类去掉
+        # image_np = np.array(image)
+        # image_np[output_np == 0] = image_src[output_np == 0]
+        # image = Image.fromarray(image_np)
+        image.save(OUTPUTS + path)
+
+        print(path + " is done!")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--class_num", type=int, default=19, help="预测的类别的种类")
+parser.add_argument("--weights", type=str, default="weights/SegNet_weights1625783040.5590682.pth", help="训练好的权重路径")
+parser.add_argument("--colors", type=int, default=[[0, 255, 0], [255, 0, 0],[0, 0, 255],[111, 74, 0],[70, 70, 70],[128, 64, 128],[0, 0, 0],[102, 102, 156],[190, 153, 153],[150, 100, 100],[107, 142, 35],
+                 [152, 251, 152],[70, 130, 180],[220, 220, 0],[119, 11, 32],[215, 166, 66],[66, 88, 99],[154, 25, 244],[10, 155, 83]], help="类别覆盖的颜色")
+parser.add_argument("--samples", type=str, default="samples//", help="用于测试的图片文件夹的路径")
+parser.add_argument("--outputs", type=str, default="outputs//", help="保存结果的文件夹的路径")
+opt = parser.parse_args()
+#print(opt)
+
+CLASS_NUM = opt.class_num
+WEIGHTS = opt.weights
+COLORS = opt.colors
+SAMPLES = opt.samples
+OUTPUTS = opt.outputs
+
+
+SegNet = SegNet(3, CLASS_NUM)
+test(SegNet)
